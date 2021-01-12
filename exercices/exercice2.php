657f4725d6a1ca57a812640b94fbc9900f4b1735
Sylvain Pellier <pellier@gmail.com>
2021-01-12 16:24:44 +0100

    exercice tableau

--- exercices/exercice2.php
+++ exercices/exercice2.php
@@ -1,31 +1,39 @@
 <?php
 
-
-
+$chambres = [];
 
 $chambre1 = [];
 $chambre1["nom"] = "La suite";
 $chambre1["taille"] = 100;
 $chambre1["service"] = ["wifi","bluetooth","douche"];
 
+$chambres[] = $chambre1;
+
 $chambre2 = [];
 $chambre2["nom"] = "La lodge";
 $chambre2["taille"] = 200;
 $chambre2["service"] = ["wifi","baignoire"];
+
+$chambres[] = $chambre2;
 
 $chambre3 = [];
 $chambre3["nom"] = "La mini";
 $chambre3["taille"] = 50;
 $chambre3["service"] = ["wifi","douche"];
 
+$chambres[] = $chambre3;
+
 //exercice 1 :
 //afficher la liste des chambres
 //<ul><li>La suite</li></li>La lodge, ...</li></ul>
+
+
 
 //exercice 2 : afficher la liste des chambres et de leur service :
 //<ul><li>La suite proposent : <ul><li>wifi</li><li>bluetooth</li></ul></li></li>La lodge, ...</li></ul>
 
 //exercice 3 : afficher la liste des chambres qui proposent une baignoire
+//<ul><li>La lodge, ...</li></ul>
 
 //exercice 4 : créer un début de site avec une page d'accueil qui affiche toutes les chambres
 //et quand on clique sur une chambre on arrive sur une nouvelle page avec toutes les informations de la chambre
--- exercices/page1.php
+++ exercices/page1.php
@@ -6,11 +6,11 @@ echo"<a href='page2.php?type=fruit'>solution abricot</a>";
 
 //exercice 2
 echo"<p>Écrire un lien qui doit ouvrir la page2.php, lorsque la page 2 s'ouvre le message suivant doit s'afficher : abricot et chien</p>";
-echo"<a href='page2.php'>solution chien</a>";
+echo"<a href='page2.php?type2=animal&type=fruit'>solution chien</a>";
 //exercice 3
 echo"<p>Écrire un lien qui doit ouvrir la page2.php, lorsque la page 2 s'ouvre le message suivant doit s'afficher : 7</p>";
-echo"<a href='page2.php'>solution 7</a>";
+echo"<a href='page2.php?calcul=addition&a=5&b=2'>solution 7</a>";
 
 //exercice 4
 echo"<p>Écrire un lien qui doit ouvrir la page2.php, lorsque la page 2 s'ouvre le message suivant doit s'afficher : php</p>";
-echo"<a href='page2.php'>solution php</a>";
+echo"<a href='page2.php?simple=php&message'>solution php</a>";
